310217dc72a52d92445ef6b1f77a54d73d0b68cf
NathanPr033 <npringle1233@btinternet.com>
2022-03-04 22:23:11 +0000

    WIP

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var ready = (callback) => {
     if (document.readyState != "loading") callback();
     else document.addEventListener("DOMContentLoaded", callback);
@@ -6,9 +7,12 @@ ready(() => {
     document.querySelector(".header").style.height = window.innerHeight + "px";
 })
 
-$('#list-home a').on('click', function (e) {
+$('#myList a').on('click', function (e) {
   e.preventDefault()
-  $(this).tab('show')
+  $(this).tabs('show')
 })
 
-$('#list-home a[href="#list-home"]').tab('show')
+$('#myList a[href="#profile"]').tab('show')
+$('#myList a[href="#messages"]').tab('show')
+$('#myList a[href="#settings"]').tab('show')
+
